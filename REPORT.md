941123cade3489ddcba093152ec66f2fbdf025f6
mbacharis <mbacharis@googlemail.com>
2017-07-23 15:51:45 +0100

    Implement Summary view and template

--- REPORT.md
+++ REPORT.md
@@ -15,3 +15,15 @@ $python manage.py shell
 It also creates an error report as there are some additional elements in some of the files same timestamp repeated twice. These second instances are saved in the error report as error_report.csv
 
 Also for test purposes there is a break in lines 67-68 that should be removed at full deployement. This was included as the loading process in my computer takes a long time, I should explore alternatives.
+
+views.py: 
+
+Bokeh is used to create the plot with the summary data. This is used as Bokeh can directly output in html and has also interactive features (to turn these off uncomment from views.py line 34).
+
+User.objects.all() is passed to the template to provide the data for the creation of the table in the template. 
+
+Templates - consumption/summary.html:
+
+The template imports some styles for Bokeh and splits the screen in two for the plot and the table. 
+
+In the first implementation the hyperlinks for the table just point to "/detail/"  
--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -4,12 +4,39 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-
+#Bokeh is used to create the plots
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import components
+from bokeh.embed import file_html
+from consumption.models import User,Summary
+#for removing the timezone information
+import pytz
 
 def summary(request):
-  context = {
-      'message': 'Hello!',
-  }
+#  context = {
+#      'message': 'Hello!',
+#  }
+  #the x_axis_type='datetime' is added to 
+  #allow Bokeh to correctly render the datetime
+  #the data can't have timezone information
+  plot = figure(width=700,height=350, responsive=True,x_axis_type='datetime')
+  xData=[]
+  yData=[]
+  for i in Summary.objects.all():
+      #removes the timezone information
+	  xData.append(i.summary_time.replace(tzinfo=None))
+      yData.append(i.summary_use)
+  plot.circle(xData, yData)
+  #Formating the plot
+  #Remove Bokeh logo and toolbars
+  plot.toolbar.logo = None
+  #plot.toolbar_location = None
+  #adds axis labels
+  plot.xaxis.axis_label='Date'
+  plot.yaxis.axis_label='Total Consumption / Wh'
+  script, div = components(plot)	  
+  context={"the_script": script, "the_div": div,"the_user":User.objects.all(),}
   return render(request, 'consumption/summary.html', context)
 
 def detail(request):
