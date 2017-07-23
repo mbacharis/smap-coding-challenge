f86254fc3549b6c1d24ae28bedb5bc6469a3718e
mbacharis <mbacharis@googlemail.com>
2017-07-23 10:52:31 +0100

    New Model.py
    implement the models for the consumption app

---
+++ REPORT.md
@@ -0,0 +1,9 @@
+M. Bacharis 
+
+The adopted approach is detailed below:
+
+models.py:
+
+I have chosen to include 3 models. The first, "Summary", is for storing summary data for all users that will be presented in the summary page. These are processesed by the load.py file, see below. The User and EnergyUse models are for loading the user and consumption data.
+
+ 
--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -1,6 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+
+# The Summary model is for loading the processed summary data from load.py
+class Summary(models.Model):
+    summary_time=models.DateTimeField()
+    summary_use=models.FloatField()
+	#This is for having readable output
+    def __str__(self):
+        output=+str(self.summary_time)+", "+str(self.summary_use)  
+        return output
+    
+# The User model is for loading the data from /data/user_data.csv
+class User(models.Model):
+    user_id=models.IntegerField()
+    user_area=models.CharField(max_length=2)
+    user_tariff=models.CharField(max_length=2)
+    def __str__(self):
+        output=str(self.user_id)+", "+self.user_area+", "+self.user_tariff
+        return output
+
+#The EnergyUse model is linked with the User model for loading data from the user files from /data/consumption
+class EnergyUse(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE) #This links the EnergyUse with the User model
+    EnergyUse_time=models.DateTimeField()
+    EnergyUse_consumption=models.FloatField()
+    def __str__(self):
+        output=str(self.user_id)+", "+str(self.EnergyUse_time)+", "+str(self.EnergyUse_consumption)  
+        return output
+        
